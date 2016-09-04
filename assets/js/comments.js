b4688bfc03ecaeee4824ea76af19d12a8a2d64dd
dejanzelic <dejanzelic@gmail.com>
2016-09-04 12:14:28 -0700

    Fixed Disqus script

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,20 +1,24 @@
-    var disqus_loaded = false;
+---
+---
 
-    function load_disqus()
-    {
-        disqus_loaded = true;
-        var disqus_shortname = '{{site.disqus_sitename}}';
-        var disqus_title = '{{page.title}}';
-        var disqus_url = '{{page.url}}';
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        var ldr = document.getElementById('disqus_loader');
-    };
-    window.onscroll = function(e) {
-        if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 800)) {
-            //hit bottom of page
-            if (disqus_loaded==false)
-                load_disqus()
-        }
-    };
+var disqus_loaded = false;
+
+function load_disqus()
+{
+    disqus_loaded = true;
+    var disqus_shortname = '{{site.disqus_sitename}}';
+    var disqus_title = document.getElementsByClassName('post-title')[0].innerHTML;
+    var disqus_url = window.location.pathname;
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    var ldr = document.getElementById('disqus_loader');
+};    
+
+window.onscroll = function(e) {
+   if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 800)) {
+        //hit bottom of page
+        if (disqus_loaded==false)
+            load_disqus()
+   }
+};
